b428fcd2a8d8665a3e65c9a2ff8efd4a335bb8f8
Ivanhola <32990933+Ivanhola@users.noreply.github.com>
2018-06-22 17:34:15 -0700

    Update VariableLengthArguments.java

--- VariableLengthArguments/VariableLengthArguments.java
+++ VariableLengthArguments/VariableLengthArguments.java
@@ -5,11 +5,13 @@ package variablelengtharguments;
 public class VariableLengthArguments {
 
     public static void main(String[] args) {
+       
        System.out.println(average(10,29,32,23,23)); //takes in all these arguments, and outputs the average
     }
     
     public static int average(int...numbers){ //when you dont know how many arguments in the parameters, put ...
         int total = 0;
+        //enhanced for loop, to get each number and make it = to X for each value, then use a total variable to add all the values.
         for(int x:numbers){
             total = total + x;
              }
